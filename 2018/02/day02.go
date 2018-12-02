bd0d460055d70bac8b557043068502aac9359028
Alvar <post@0x21.biz>
2018-12-02 10:32:01 +0100

    Day 02: part two

--- 2018/02/day02.go
+++ 2018/02/day02.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 )
@@ -41,6 +42,26 @@ func strToOccurrenceMap(str string) map[rune]uint {
 	return occurrenceMap
 }
 
+func strRuneCompare(str1, str2 string) (diffs uint, same string, err error) {
+	diffs = 0
+	same = ""
+
+	if len(str1) != len(str2) {
+		err = errors.New("strings differ in length")
+		return
+	}
+
+	for i, c := range str1 {
+		if str1[i] == str2[i] {
+			same += string(c)
+		} else {
+			diffs += 1
+		}
+	}
+
+	return
+}
+
 func partOne() {
 	var twos, threes uint = 0, 0
 
@@ -64,6 +85,30 @@ func partOne() {
 	fmt.Printf("Result: %d * %d = %d\n\n", twos, threes, twos*threes)
 }
 
+func partTwo() {
+	lines := linewiseInput("input")
+
+	fmt.Println("--- Part Two ---")
+
+	for i := 0; i < len(lines); i++ {
+		for j := 0; j < i; j++ {
+			diffs, same, err := strRuneCompare(lines[i], lines[j])
+			if err != nil {
+				panic(err)
+			}
+
+			if diffs == 1 {
+				fmt.Printf("%v and %v differ only by once,\nresulting in %v\n\n",
+					lines[i], lines[j], same)
+				return
+			}
+		}
+	}
+
+	fmt.Println("Found nothing ;_;")
+}
+
 func main() {
 	partOne()
+	partTwo()
 }
